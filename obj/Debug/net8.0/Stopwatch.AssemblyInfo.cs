207c56608f0593aa4901fcd868261ff391e34d33
Matheus <matheus.ferraro@gmail.com>
2024-12-29 19:32:11 -0500

    fuck

--- obj/Debug/net8.0/Stopwatch.AssemblyInfo.cs
+++ obj/Debug/net8.0/Stopwatch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54ea88b278266cd42e09a56d6febf9de0fcb3507")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
